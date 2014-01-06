e0b2b8d5713b404e21e9faf68c0e3a54f2f13e41
saerem21 <nemessisdds>
2014-01-06 19:00:10 +0900

    테이블뷰 추가하기 + 이동 하는 예제입니다.
    page 188
    텍스트를 못 받아옵니다…

--- Ex188_ex14/ViewController.m
+++ Ex188_ex14/ViewController.m
@@ -6,19 +6,94 @@
 //  Copyright (c) 2014년 SDT-1. All rights reserved.
 //
 
-#import "ViewController.h"
 
-@interface ViewController ()
+#import "ViewController.h"
+#define CELL_ID @"CELL_ID"
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate>
+{
+    NSMutableArray *data;
+}
+@property (weak, nonatomic) IBOutlet UITextField *userInput;
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
 @implementation ViewController
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [data count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CELL_ID"];
+    cell.textLabel.text = [NSString stringWithFormat:@"cell %d",indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(UITableViewCellEditingStyleDelete == editingStyle){
+        [data removeObjectAtIndex:indexPath.row];
+        NSArray *rowList = [NSArray arrayWithObject :indexPath];
+        [tableView deleteRowsAtIndexPaths:rowList withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+}
+
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    NSObject *obj = [data objectAtIndex:sourceIndexPath.row];
+    [data removeObjectAtIndex:sourceIndexPath.row];
+    [data insertObject:obj atIndex:destinationIndexPath.row];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    data =[[NSMutableArray alloc]init];
 }
+
+
+- (IBAction)addItem:(id)sender {
+    NSString *inputStr = self.userInput.text;
+    if([inputStr length] > 0){
+        [data addObject:inputStr];
+    
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:([data count]-1) inSection:0];
+    NSArray *row = [NSArray arrayWithObject:indexPath];
+    
+        [self.table insertRowsAtIndexPaths:row withRowAnimation:UITableViewRowAnimationAutomatic];
+    self.userInput.text=@"";
+    }
+}
+
+- (IBAction)toggleEdit:(id)sender {
+    self.table.editing = !self.table.editing;
+    ((UIBarButtonItem *)sender).title = self.table.editing ? @"done" : @"edit";
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    [self addItem:nil];
+    return YES;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -27,3 +102,4 @@
 }
 
 @end
+
